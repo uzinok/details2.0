0494280957691e2048c949801882c5d1dc91ebe9
uzinok <41639106+uzinok@users.noreply.github.com>
2022-05-04 17:47:08 +0300

    готово

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,49 @@
-console.log('hi!xx');
-const hello = 'hello';
-let bue = 'bue';
-document.write(hello);
-class Buka {
-	constructor(){}
+class Details {
+	constructor(details, transitionDuration) {
+		this._details = details;
+		this._summary = details.querySelector('.details__summary');
+		this._content = details.querySelector('.details__content');
+
+		this.transitionDuration = transitionDuration;
+
+		this.initD();
+	}
+
+	initD() {
+		if (this._details.open)
+			this.closeD();
+		else
+			this.openD();
+
+		this._summary.addEventListener('click', (event) => {
+			event.preventDefault();
+
+			if (this._details.open)
+				this.closeD();
+			else
+				this.openD();
+		});
+	}
+
+	closeD() {
+		this._details.style.height = this._summary.offsetHeight + 'px';
+		this._details.classList.remove('details--open');
+
+		setTimeout(() => {
+			this._details.open = false;
+		}, this.transitionDuration);
+	}
+
+	openD() {
+		this._details.open = true;
+		this._details.classList.add('details--open');
+		this._details.style.height = this._summary.offsetHeight + this._content.offsetHeight + 'px';
+	}
+
 }
-// alert('hi!!!!!!!!!!!')
+
+if (document.querySelector('.details')) {
+	document.querySelectorAll('.details').forEach(elem => {
+		let details = new Details(elem, 300);
+	})
+}
